5ddb37259817808a3bc179a5c90d54d077f21280
Chun-da Chen <capitalm.c@gmail.com>
2017-09-07 17:13:09 +0800

    Initial import.

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+
+base_dir = os.path.dirname(__file__)
+setup(
+    name='thingnario-elastalert',
+    version='0.1.0',
+    description='Customized Elastalert rule typs and alerts',
+    author='Chun-da Chen',
+    author_email='capitalm@thingnario.com',
+    setup_requires='setuptools',
+    license='Copyright 2017 thingnario LTD.',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={},
+    packages=['thingnario_elastalert'],
+    package_data={},
+    install_requires=[
+        'elastalert>=0.1.17',
+        'pytz>=2017.2',
+        'python-dateutil==2.6.1',
+    ]
+)
---
+++ thingnario_elastalert/alerter.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from datetime import time
+from dateutil import parser
+from elastalert import alerts
+from pytz import timezone
+
+
+utc = timezone('UTC')
+
+
+def parse_time(s):
+    if not s:
+        return None
+    return time(*tuple(map(int, s.split(':'))))
+
+
+class InTimeframe(object):
+    def __init__(self, timestamp_field, schedule):
+        self.timestamp_field = timestamp_field
+        self.tz = timezone(schedule.get('timezone', 'UTC'))
+        self.from_ = parse_time(schedule.get('from', None))
+        self.to = parse_time(schedule.get('to', None))
+
+    def __call__(self, entry):
+        dt = parser.parse(entry[self.timestamp_field])
+        if not dt.tzinfo:
+            dt = utc.localize(dt)
+        t = dt.astimezone(self.tz).time()
+        if self.from_ and t < self.from_:
+            return False
+        if self.to and t > self.to:
+            return False
+        return True
+
+
+class ScheduledAlerter(object):
+    def __init__(self, rule):
+        self.alerter = next((x for x in self.__class__.__bases__
+                             if issubclass(x, alerts.Alerter)),
+                            None)
+        if self.alerter:
+            self.alerter.__init__(self, rule)
+
+        self.in_timeframe = InTimeframe(
+            timestamp_field=rule['timestamp_field'],
+            schedule=rule.get('schedule', {}))
+
+    def alert(self, matches):
+        matches = filter(self.in_timeframe, matches)
+        if matches:
+            alerts.DebugAlerter.alert(self, matches)
+
+
+class ScheduledDebugAlerter(ScheduledAlerter, alerts.DebugAlerter):
+    pass
