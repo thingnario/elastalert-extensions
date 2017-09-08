4e2d70872200968a3cbeffaf266de29b74c20736
Chun-da Chen <capitalm.c@gmail.com>
2017-09-08 12:03:09 +0800

    Rename to elastalert-extensions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 base_dir = os.path.dirname(__file__)
 setup(
-    name='thingnario-elastalert',
+    name='elastalert-extensions',
     version='0.1.0',
     description='Customized Elastalert rule typs and alerts',
     author='Chun-da Chen',
@@ -18,7 +18,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     entry_points={},
-    packages=['thingnario_elastalert'],
+    packages=['elastalert_extensions'],
     package_data={},
     install_requires=[
         'elastalert>=0.1.17',
