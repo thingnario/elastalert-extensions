f247b20d7b850dc6adcc4809eb2192dff015221d
Chun-da Chen <capitalm.c@gmail.com>
2017-09-27 17:11:58 +0800

    Add profiled flat-line rule type.

--- elastalert_extensions/ruletypes.py
+++ elastalert_extensions/ruletypes.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
+import copy
+from datetime import timedelta
+import json
+import os.path
+import time
+
+from elastalert.util import dt_to_ts
+from elastalert.util import EAException
+from elastalert.util import elastalert_logger
+from elastalert.util import hashable
+from elastalert.util import lookup_es_key
+from elastalert.util import new_get_event_ts
+from elastalert.util import pretty_ts
+from elastalert.util import ts_to_dt
 from elastalert.ruletypes import BlacklistRule, CompareRule
-from elastalert.util import hashable, lookup_es_key, elastalert_logger
+from elastalert.ruletypes import EventWindow
+from elastalert.ruletypes import FrequencyRule
+
+
+UPDATE_INTERVAL = 60.0
 
 
 class BlacklistDurationRule(CompareRule):
@@ -82,3 +100,181 @@ class CompoundBlacklistRule(BlacklistRule):
             if term in self.rules['blacklist']:
                 return True
         return False
+
+
+class ProfiledFrequencyRule(FrequencyRule):
+    """ A rule that matches if num_events number of events occur within a timeframe """
+    required_options = frozenset(['num_events', 'timeframe', 'profile'])
+
+    def __init__(self, *args):
+        super(ProfiledFrequencyRule, self).__init__(*args)
+        self.ts_field = self.rules.get('timestamp_field', '@timestamp')
+        self.get_ts = new_get_event_ts(self.ts_field)
+        self.attach_related = self.rules.get('attach_related', False)
+        self._profile = {}
+        self._profile_ts = 0.0
+        self._update_ts = 0.0
+
+    def timeframe(self, key):
+        return self.profile.get(key, self.rules['timeframe'])
+
+    @property
+    def profile(self):
+        profile_path = self.rules['profile']
+        now = time.time()
+
+        try:
+            if now > self._update_ts + UPDATE_INTERVAL:
+                ts = os.path.getmtime(profile_path)
+            else:
+                ts = self._profile_ts
+
+            if ts > self._profile_ts:
+                elastalert_logger.info('Reloading profile %s', profile_path)
+                with open(profile_path, 'r') as profile_file:
+                    profile = json.load(profile_file)
+                    self._profile = {k: timedelta(**profile[k])
+                                     for k in profile}
+                    self._profile_ts = ts
+                    self._update_ts = now
+        except (OSError, IOError, ValueError) as e:
+            elastalert_logger.error('Cannot load profile %s: %s', profile_path, e)
+        return self._profile
+
+    def add_count_data(self, data):
+        """ Add count data to the rule. Data should be of the form {ts: count}. """
+        if len(data) > 1:
+            raise EAException('add_count_data can only accept one count at a time')
+
+        (ts, count), = data.items()
+
+        event = ({self.ts_field: ts}, count)
+        self.occurrences.setdefault(
+            'all',
+            EventWindow(self.timeframe('all'), getTimestamp=self.get_ts)
+        ).append(event)
+        self.check_for_match('all')
+
+    def add_terms_data(self, terms):
+        for timestamp, buckets in terms.iteritems():
+            for bucket in buckets:
+                event = ({self.ts_field: timestamp,
+                          self.rules['query_key']: bucket['key']}, bucket['doc_count'])
+                self.occurrences.setdefault(
+                    bucket['key'],
+                    EventWindow(self.timeframe(bucket['key']), getTimestamp=self.get_ts)
+                ).append(event)
+                self.check_for_match(bucket['key'])
+
+    def add_data(self, data):
+        if 'query_key' in self.rules:
+            qk = self.rules['query_key']
+        else:
+            qk = None
+
+        for event in data:
+            if qk:
+                key = hashable(lookup_es_key(event, qk))
+            else:
+                # If no query_key, we use the key 'all' for all events
+                key = 'all'
+
+            # Store the timestamps of recent occurrences, per key
+            self.occurrences.setdefault(
+                key, EventWindow(self.timeframe(key), getTimestamp=self.get_ts)).append((event, 1))
+            self.check_for_match(key, end=False)
+
+        # We call this multiple times with the 'end' parameter because subclasses
+        # may or may not want to check while only partial data has been added
+        if key in self.occurrences:  # could have been emptied by previous check
+            self.check_for_match(key, end=True)
+
+    def garbage_collect(self, timestamp):
+        """ Remove all occurrence data that is beyond the timeframe away """
+        stale_keys = []
+        for key, window in self.occurrences.iteritems():
+            if timestamp - lookup_es_key(window.data[-1][0], self.ts_field) > self.timeframe(key):
+                stale_keys.append(key)
+        map(self.occurrences.pop, stale_keys)
+
+    def get_match_str(self, match):
+        lt = self.rules.get('use_local_time')
+        match_ts = lookup_es_key(match, self.ts_field)
+        key = match.get('key', 'all')
+        starttime = pretty_ts(dt_to_ts(ts_to_dt(match_ts) - self.timeframe(key)), lt)
+        endtime = pretty_ts(match_ts, lt)
+        message = 'At least %d events occurred between %s and %s\n\n' % (self.rules['num_events'],
+                                                                         starttime,
+                                                                         endtime)
+        message = json.dumps(match)
+        return message
+
+
+class ProfiledFlatlineRule(ProfiledFrequencyRule):
+    """ A rule that matches when there is a low number of events given a timeframe. """
+    required_options = frozenset(['timeframe', 'threshold'])
+
+    def __init__(self, *args):
+        super(ProfiledFlatlineRule, self).__init__(*args)
+        self.threshold = self.rules['threshold']
+
+        # Dictionary mapping query keys to the first events
+        self.first_event = {}
+
+    def check_for_match(self, key, end=True):
+        # This function gets called between every added document with end=True after the last
+        # We ignore the calls before the end because it may trigger false positives
+        if not end:
+            return
+
+        most_recent_ts = self.get_ts(self.occurrences[key].data[-1])
+        if self.first_event.get(key) is None:
+            self.first_event[key] = most_recent_ts
+
+        # Don't check for matches until timeframe has elapsed
+        if most_recent_ts - self.first_event[key] < self.timeframe(key):
+            return
+
+        # Match if, after removing old events, we hit num_events
+        count = self.occurrences[key].count()
+        if count < self.rules['threshold']:
+            # Do a deep-copy, otherwise we lose the datetime type
+            # in the timestamp field of the last event
+            event = copy.deepcopy(self.occurrences[key].data[-1][0])
+            event.update(key=key, count=count)
+            self.add_match(event)
+
+            # After adding this match, leave the occurrences windows alone since it will
+            # be pruned in the next add_data or garbage_collect, but reset the first_event
+            # so that alerts continue to fire until the threshold is passed again.
+            least_recent_ts = self.get_ts(self.occurrences[key].data[0])
+            timeframe_ago = most_recent_ts - self.timeframe(key)
+            self.first_event[key] = min(least_recent_ts, timeframe_ago)
+
+    def get_match_str(self, match):
+        ts = match[self.rules['timestamp_field']]
+        lt = self.rules.get('use_local_time')
+        key = match.get('key', 'all')
+        message = 'An abnormally low number of events occurred around %s.\n' % (pretty_ts(ts, lt))
+        message += 'Between %s and %s, there were less than %s events.\n\n' % (
+            pretty_ts(dt_to_ts(ts_to_dt(ts) - self.timeframe(key)), lt),
+            pretty_ts(ts, lt),
+            self.rules['threshold']
+        )
+        message = json.dumps(match)
+        return message
+
+    def garbage_collect(self, ts):
+        # We add an event with a count of zero to the EventWindow for each key.
+        # This will cause the EventWindow to remove events that occurred
+        # more than one `timeframe` ago, and call onRemoved on them.
+        default = ['all'] if 'query_key' not in self.rules else []
+        for key in self.occurrences.keys() or default:
+            self.occurrences.setdefault(
+                key,
+                EventWindow(self.timeframe(key), getTimestamp=self.get_ts)
+            ).append(
+                ({self.ts_field: ts}, 0)
+            )
+            self.first_event.setdefault(key, ts)
+            self.check_for_match(key)
